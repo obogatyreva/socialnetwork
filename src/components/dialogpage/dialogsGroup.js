42f69440501095f237e279e72a2fed53cd7107ac
Olga Bogatyreva <helga_bogatyreva@mail.ru>
2018-12-30 13:41:43 +0300

    Refactoring App & dialogsGroup.

--- src/components/dialogpage/dialogsGroup.js
+++ src/components/dialogpage/dialogsGroup.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
 const DialogsGroup = (props) => {
-    let groups = props.config.data.components.dialogs.map((item) =>{
-        return <li>{item.name}</li>;
-    });
+  let groups = props.config.data.components.dialogs.map((item) =>{
+    return <li>{item.name}</li>;
+  });
 
-    return (
-        <div className = 'group-list'>
-           <ul>
-               {groups}
-           </ul>
-        </div>
-    )
+  return (
+    <div className = 'group-list'>
+      <ul>
+        {groups}
+      </ul>
+    </div>
+  )
 }
 
 export default DialogsGroup;
